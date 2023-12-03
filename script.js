8c9919ae3126bf6ab86afe0d84786a6112d93e45
VladKojima <102826875+VladKojima@users.noreply.github.com>
2023-12-03 20:37:20 +0400

    added vals

--- script.js
+++ script.js
@@ -4,16 +4,19 @@ function getObject(){
         info2:{
             value:'value',
             property:{
-                
+
             }
         },
         property2: {
             value: 'value',
             info: [12,3,21,2]
-        }
+        },
+
+        vals: [1,2,3,4,5,6,7,8,-1,2,3]
     };
 
     return obj;
 }
 
 console.log(getObject().info[1]);
+
