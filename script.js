35bfc56220c97ea1127d625b00f7670533c4b6bd
VladKojima <102826875+VladKojima@users.noreply.github.com>
2023-12-03 20:36:34 +0400

    changes2

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function getObject(){
     let obj = {
-        info: [1,2,3],
+        //info: [1,2,3],
+        info2:{
+            value:'value',
+            property:{
+                
+            }
+        },
         property2: {
             value: 'value',
             info: [12,3,21,2]
